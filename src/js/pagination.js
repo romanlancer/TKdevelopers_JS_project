d96fd2f78e8ce2823df88d131bd1942f57ddaba6
Roman Hyrych <roman.girich@gmail.com>
2022-03-30 20:35:34 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,4 @@
-
-import {createElement} from './createElement';
+import { createElement } from './createElement';
 
 export default class Pagination {
 	#currentPage = 1;
@@ -21,7 +20,6 @@ export default class Pagination {
 		if (this.onChange) {
 			this.onChange(value);
 		}
-		
 
 		// опциональный вызов функции
 		// onChange?.(value)
@@ -44,24 +42,34 @@ export default class Pagination {
 	}
 
 	createPaginationList() {
-  	let beforePage = this.currentPage - 2;
+		let beforePage = this.currentPage - 2;
 		let afterPage = this.currentPage + 2;
-		
+
 		this.paginationList = [];
 
-		if(this.currentPage === 1){ 
-			const firstElem = createElement('li', { class: 'first-num pagination-item pagination-number active', }, createElement('span', {}, '1'));
+		if (this.currentPage === 1) {
+			const firstElem = createElement(
+				'li',
+				{ class: 'first-num pagination-item pagination-number active' },
+				createElement('span', {}, '1'),
+			);
+			this.paginationList.push(firstElem);
+		} else {
+			const firstElem = createElement(
+				'li',
+				{ class: 'first-num pagination-item pagination-number' },
+				createElement('span', {}, '1'),
+			);
 			this.paginationList.push(firstElem);
 		}
-		else { 
-			const firstElem = createElement('li', { class: 'first-num pagination-item pagination-number', }, createElement('span', {}, '1'));
-			this.paginationList.push(firstElem);
-    }
-		
-		
+
 		if (this.currentPage > 4) {
-			const startDots = createElement('li', { class: 'pagination-item pagination-dots', },createElement('span', {}, '...')); 
-		this.paginationList.push(startDots);
+			const startDots = createElement(
+				'li',
+				{ class: 'pagination-item pagination-dots' },
+				createElement('span', {}, '...'),
+			);
+			this.paginationList.push(startDots);
 		}
 		if (this.currentPage === 1) {
 			afterPage = this.currentPage + 4;
@@ -69,7 +77,7 @@ export default class Pagination {
 		if (this.currentPage === 2) {
 			afterPage = this.currentPage + 3;
 		}
-    if (this.currentPage === this.total) {
+		if (this.currentPage === this.total) {
 			beforePage = this.currentPage - 4;
 		}
 		if (this.currentPage === this.total - 1) {
@@ -79,32 +87,49 @@ export default class Pagination {
 		if (beforePage < 2) {
 			beforePage = 2;
 		}
-		if (afterPage > this.total) {
+		if (afterPage >= this.total) {
 			afterPage = this.total - 1;
 		}
-		for (let pageNumber = beforePage; pageNumber <= afterPage; pageNumber++){ 
-
-			if(pageNumber === this.currentPage){ 
-				const item = createElement('li', { class: 'pagination-item pagination-number active', }, createElement('span', {}, `${pageNumber}`));
-			this.paginationList.push(item);
-			}
-			else { 
-				const item = createElement('li', { class: 'pagination-item pagination-number', }, createElement('span', {}, `${pageNumber}`));
-			this.paginationList.push(item);
+		for (let pageNumber = beforePage; pageNumber <= afterPage; pageNumber++) {
+			if (pageNumber === this.currentPage) {
+				const item = createElement(
+					'li',
+					{ class: 'pagination-item pagination-number active' },
+					createElement('span', {}, `${pageNumber}`),
+				);
+				this.paginationList.push(item);
+			} else {
+				const item = createElement(
+					'li',
+					{ class: 'pagination-item pagination-number' },
+					createElement('span', {}, `${pageNumber}`),
+				);
+				this.paginationList.push(item);
 			}
 		}
 
 		if (this.currentPage < this.total - 3) {
-			const endDots = createElement('li', { class: 'pagination-item pagination-dots', },createElement('span', {}, '...')); 
-		this.paginationList.push(endDots);
+			const endDots = createElement(
+				'li',
+				{ class: 'pagination-item pagination-dots' },
+				createElement('span', {}, '...'),
+			);
+			this.paginationList.push(endDots);
 		}
 
 		if (this.currentPage === this.total) {
-			const lastElem = createElement('li', { class: 'last-num pagination-item pagination-number active', }, createElement('span', {}, `${this.total}`));
+			const lastElem = createElement(
+				'li',
+				{ class: 'last-num pagination-item pagination-number active' },
+				createElement('span', {}, `${this.total}`),
+			);
 			this.paginationList.push(lastElem);
-		}
-		else {
-			const lastElem = createElement('li', { class: 'last-num pagination-item pagination-number', }, createElement('span', {}, `${this.total}`));
+		} else {
+			const lastElem = createElement(
+				'li',
+				{ class: 'last-num pagination-item pagination-number' },
+				createElement('span', {}, `${this.total}`),
+			);
 			this.paginationList.push(lastElem);
 		}
 		return this.paginationList;
