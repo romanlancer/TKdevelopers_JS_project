87d600770a6d598390cba7c41c12da21c5d642f1
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-03-30 14:28:14 +0300

    modified render_search and add render pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,5 @@
-import MoviesApiService from './fetch_api';
+
+import {createElement} from './createElement';
 
 export default class Pagination {
 	#currentPage = 1;
@@ -7,6 +8,7 @@ export default class Pagination {
 		this.#currentPage = initialPage;
 		this.total = total;
 		this.onChange = onChange;
+		this.paginationList = [];
 	}
 
 	get currentPage() {
@@ -19,6 +21,7 @@ export default class Pagination {
 		if (this.onChange) {
 			this.onChange(value);
 		}
+		
 
 		// опциональный вызов функции
 		// onChange?.(value)
@@ -38,5 +41,72 @@ export default class Pagination {
 		}
 
 		this.currentPage -= 1;
+	}
+
+	createPaginationList() {
+  	let beforePage = this.currentPage - 2;
+		let afterPage = this.currentPage + 2;
+		
+		this.paginationList = [];
+
+		if(this.currentPage === 1){ 
+			const firstElem = createElement('li', { class: 'first-num pagination-item pagination-number active', }, createElement('span', {}, '1'));
+			this.paginationList.push(firstElem);
+		}
+		else { 
+			const firstElem = createElement('li', { class: 'first-num pagination-item pagination-number', }, createElement('span', {}, '1'));
+			this.paginationList.push(firstElem);
+    }
+		
+		
+		if (this.currentPage > 4) {
+			const startDots = createElement('li', { class: 'pagination-item pagination-dots', },createElement('span', {}, '...')); 
+		this.paginationList.push(startDots);
+		}
+		if (this.currentPage === 1) {
+			afterPage = this.currentPage + 4;
+		}
+		if (this.currentPage === 2) {
+			afterPage = this.currentPage + 3;
+		}
+    if (this.currentPage === this.total) {
+			beforePage = this.currentPage - 4;
+		}
+		if (this.currentPage === this.total - 1) {
+			beforePage = this.currentPage - 3;
+		}
+
+		if (beforePage < 2) {
+			beforePage = 2;
+		}
+		if (afterPage > this.total) {
+			afterPage = this.total - 1;
+		}
+		for (let pageNumber = beforePage; pageNumber <= afterPage; pageNumber++){ 
+
+			if(pageNumber === this.currentPage){ 
+				const item = createElement('li', { class: 'pagination-item pagination-number active', }, createElement('span', {}, `${pageNumber}`));
+			this.paginationList.push(item);
+			}
+			else { 
+				const item = createElement('li', { class: 'pagination-item pagination-number', }, createElement('span', {}, `${pageNumber}`));
+			this.paginationList.push(item);
+			}
+		}
+
+		if (this.currentPage < this.total - 3) {
+			const endDots = createElement('li', { class: 'pagination-item pagination-dots', },createElement('span', {}, '...')); 
+		this.paginationList.push(endDots);
+		}
+
+		if (this.currentPage === this.total) {
+			const lastElem = createElement('li', { class: 'last-num pagination-item pagination-number active', }, createElement('span', {}, `${this.total}`));
+			this.paginationList.push(lastElem);
+		}
+		else {
+			const lastElem = createElement('li', { class: 'last-num pagination-item pagination-number', }, createElement('span', {}, `${this.total}`));
+			this.paginationList.push(lastElem);
+		}
+		return this.paginationList;
 	}
 }
--- src/js/render_search.js
+++ src/js/render_search.js
@@ -5,12 +5,19 @@ import { generateImgPath } from './generate_img_path';
 import moviesCardTemplate from '../templates/movie_cards.hbs';
 
 const moviesListRef = document.querySelector('[data-list]');
+const paginationListRef = document.querySelector('.pagination .pagination-list');
 const nextPageRef = document.querySelector('.next-page');
 const prevPageRef = document.querySelector('.prev-page');
-const currentPageRef = document.querySelector('.current-page');
 const searchFormRef = document.querySelector('#search-form');
 
 const moviesApiService = new MoviesApiService();
+
+const moviePagination = new Pagination({
+		total: 1,
+		onChange(value) {
+			handlePageChange(value);
+		},
+	});
 
 Notiflix.Notify.init({
 	position: 'top-right',
@@ -20,6 +27,21 @@ Notiflix.Notify.init({
 
 searchFormRef.addEventListener('submit', onSearch);
 
+nextPageRef.addEventListener('click', () => {
+		moviePagination.nextPage();
+	});
+
+prevPageRef.addEventListener('click', () => {
+		moviePagination.prevPage();
+	});
+
+paginationListRef.addEventListener('click', (event) => {
+	if (event.target.parentNode.classList.contains('pagination-number') && !event.target.parentNode.classList.contains('active')) {
+		const clickPage = parseInt(event.target.textContent);
+		moviePagination.currentPage = clickPage;
+	}
+});
+
 function onSearch(e) {
 	e.preventDefault();
 	moviesApiService.query = e.currentTarget.elements.searchQuery.value;
@@ -27,16 +49,15 @@ function onSearch(e) {
 	if (moviesApiService.query === '') {
 		return Notiflix.Notify.failure('Wrong attempt bro, please enter something');
 	}
-	moviesApiService.resetPage();
 	clearMoviesContainer();
-	const moviePagination = new Pagination({
-		total: 100,
-		onChange(value) {
-			handlePageChange(value);
-			currentPageRef.textContent = value;
-		},
-	});
-	const renderMovieList = movies => {
+	moviePagination.currentPage = 1;
+}
+
+function clearMoviesContainer() {
+	moviesListRef.innerHTML = '';
+}
+
+function renderMovieList (movies) {
 		const moviesList = movies.map(movie => {
 			const { original_title, poster_path } = movie;
 
@@ -50,34 +71,34 @@ function onSearch(e) {
 			'beforeend',
 			moviesCardTemplate(moviesList),
 		);
-	};
-	moviesApiService.getFilmsByName().then(({ data }) => {
-		const { results: movies } = data;
-
-		renderMovieList(movies.slice(0, 9));
-		console.log(data);
-	});
-
-	const handlePageChange = currentPage => {
-		moviesApiService.getFilmsByName(currentPage).then(({ data }) => {
+}
+	
+function handlePageChange(currentPage) {
+		moviesApiService.page = currentPage;
+		moviesApiService.getFilmsByName().then(({ data }) => {
 			clearMoviesContainer();
+			renderMovieList(data.results.slice(0, 3));
+			moviePagination.total = data.total_pages;
 
-			renderMovieList(data.results.slice(0, 9));
-			console.log(data);
+		if (currentPage === 1) {
+		prevPageRef.setAttribute('disabled', true);
+		prevPageRef.classList.add('disabled');
+		}
+		else {
+			prevPageRef.removeAttribute('disabled');
+			prevPageRef.classList.remove('disabled');
+		}
+		if (currentPage === moviePagination.total) {
+			nextPageRef.setAttribute('disabled', true);
+			nextPageRef.classList.add('disabled');
+		}
+		else {
+			nextPageRef.removeAttribute('disabled');
+			nextPageRef.classList.remove('disabled');
+		}
+
+			const paginationElem = moviePagination.createPaginationList();
+			paginationListRef.innerHTML = '';
+			paginationListRef.append(...paginationElem);
 		});
-	};
-
-	nextPageRef.addEventListener('click', () => {
-		moviePagination.nextPage();
-		moviesApiService.incrementPage();
-	});
-
-	prevPageRef.addEventListener('click', () => {
-		moviePagination.prevPage();
-		moviesApiService.decrementPage();
-	});
-}
-
-function clearMoviesContainer() {
-	moviesListRef.innerHTML = '';
-}
+	}
